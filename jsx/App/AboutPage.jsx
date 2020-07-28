02b88985cacd8fff276567898711bbf67e5148ff
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-28 10:44:56 -0400

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -4,14 +4,14 @@ export function AboutPage() {
   return (
 		<div style={{margin: "0.25in"}}>
 
-      		<h1>About ALDP <br />
-			<i style={{fontSize: "medium"}}>English</i></h1>
+      		<h2>About ALDP <br />
+			<i style={{fontSize: "medium"}}>English</i></h2>
 
 			<p><b>A’ingae Language Documentation Project</b> is an interdisciplinary effort which aims to document, investigate, and revitalize A’ingae (Cofán), a language isolate spoken by approximately 2,000 Cofán (A’i) people in the Amazonian region of Northeast Ecuador and Southern Colombia. The project is directed by Scott AnderBois (Brown University) and Wilson de Lima Silva (University of Arizona) and carried out in close collaboration with members of the Zábalo, Dureno, Dovuno, and Sinangoé communities in Ecuador.</p>
 
 
 
-			<h2>The Cofán people</h2>
+			<h3>The Cofán people</h3>
 
 			<p>The Cofán people have long lived at the intersection of two distinct cultural and geographic regions: the Andes and Amazonia. While they presently live in what is now the province of Sucumbíos in northeast Ecuador and southern Colombia, they have historically made use of a much larger territory as a primarily hunter-gatherer people. Although their numbers have been greatly reduced since the arrival of Spanish in the 16th century—down at one point to 350 people from an estimated pre-colonial population of 15,000—the Cofán now are about 2,000 strong and growing, a success they often credit to social cohesion enabled by their distinct linguistic identity. New archeological findings casting light on their material culture, its geographical extent, and the diachronic changes thereof are still being made.</p>
 
@@ -21,14 +21,14 @@ export function AboutPage() {
 
 
 
-			<h2>The A’ingae language</h2>
+			<h3>The A’ingae language</h3>
 
 			<p>While the language has more commonly been referred to by the name “Cofán,” or occasionally “Kofán” in scholarly literature, “A’ingae” is the name speakers use for their language (<i>a’ingae</i> literally means “in the way of the A’i/people”). A’ingae is a language isolate, which means that it is believed to be unrelated to any other known language on earth. Despite having no genetic relationship to other languages, A'ingae has many typological features characteristic of other languages of the region, including SOV basic word order, nominal classifiers, presence of frustative, apprehensive, and evidential morphology, highly complex agglutinative morphology, contrastive nasality on vowels and other prototypically Amazonian phonological features.</p>
 
 			<p>A’ingae is an endangered language thought to have around 2,000 native speakers in Ecuador and Colombia. The national borders are thought to demarcate a linguistic divide: of the two major dialects, one is spoken along the Aguarico River in Ecuador; the other—on the San Miguel, Guamués, and Putumayo rivers in Colombia. As the Cofán population nearly quintupled since the 1960s and A’ingae is still used vigorously, the language is classified as developing. In Ecuador, A’ingae is spoken widely though not universally across registers, by all social groups and in all domains of life, with the attitudes towards the language being positive. In communities with stronger Kichwa and colonial influences, especially in Colombia, the prominence of Spanish and other languages may be more prominent. </p>
 
 			<p>An orthography for the language was first developed by the SIL missionaries Marlytte and Roberta Borman, and more recently revised by members of the Cofán communities, which we follow here. Our team is currently in the process of conducting a sociolinguistic survey to assess the vitality of the language in written and spoken forms across different social settings.</p>
-			<h2>The A’ingae Language Documentation Project</h2>
+			<h3>The A’ingae Language Documentation Project</h3>
 
 			<p>ALDP came about as a collaboration between Scott AnderBois, a professor at Brown University, and Hugo Lucitante, a native speaker of A’ingae from Zábalo who did his BA at Brown from 2015-2019 (currently a PhD student in Anthropology at the University of Texas at San Antonio). Through thinking about the importance of his language both through his coursework as well as in conversations outside the classroom, Hugo recognized the need for community-engaged language documentation. Together with Wilson Silva, a professor at the University of Arizona, the ALDP was born.</p>
 			<p>Our project aims to document the A’ingae language, investigate scientific questions about its grammar and usage, and to promote community capacity building in service of the production of community-oriented materials.</p>
@@ -37,98 +37,99 @@ export function AboutPage() {
 
 
 
-			<h2>Members</h2>
+			<h3>Members</h3>
 
-			<h3>Co-directors</h3>
+			<h4>Co-directors</h4>
 
-			<h4>Scott anderBois</h4>
+			<h5>Scott anderBois</h5>
 			<p><a href="http://research.clps.brown.edu/anderbois/">research.clps.brown.edu/anderbois</a><br />
-			<a href="mailto:scott_anderbois@brown.edu">scott_anderbois@brown.edu</a></p>
+			scott_anderbois at brown.edu</p>
 			<p>Scott is an Associate Professor in the Department of Cognitive, Linguistic, and Psychological Sciences (CLPS) at Brown University. His research is focused on issues in semantics and pragmatics, especially those concerning non-truth conditional meanings and the way they interact with sentence type. Beyond A’ingae, his work has explored these issues through primary fieldwork on Yucatec Maya (Mayan, Mexico) and Tagalog (Austronesian, Philippines). Additionally, he co-directs with Miguel Oscar Chan Dzul (Universidad de Oriente) <a href="https://yucatecmaya.github.io/LingView/#/">U koorpusil maaya t'aan</a>, a multipurpose digital corpus of Yucatec Maya.</p>
 
-			<h4>Wilson Silva</h4>
-			<p><a href="mailto:wdelimasilva@email.arizona.edu">wdelimasilva@email.arizona.edu</a></p>
+			<h5>Wilson Silva</h5>
+			wdelimasilva at email.arizona.edu</p>
 			<p>Wilson is an Assistant Professor in the Department of Linguistics at the University of Arizona. He is a field linguist with formal training in theoretical linguistics and language documentation & revitalization. Beyond A’ingae, he has conducted research in Desano and Siriano; two endangered Eastern Tukanoan languages spoken in the Vaupés Region of Brazil and Colombia.</p>
 
 
-			<h3>Community partners</h3>
+			<h4>Community partners</h4>
 
-			<h4>Hugo Lucitante</h4>
+			<h5>Hugo Lucitante</h5>
 			<p>Hugo Lucitante is a PhD student in Anthropology at UT San Antonio and a Brown University graduate from Zábalo, a small Cofán community in Ecuador. He received the Royce Fellowship in 2016 and developed the. Hugo's work was immensely important in starting this project.</p>
 
-			<h4>Sadie Lucitante</h4>
+			<h5>Sadie Lucitante</h5>
 
-			<h4>Shen Aguinda</h4>
+			<h5>Shen Aguinda</h5>
 
-			<h4>Leidy Quenamá</h4>
+			<h5>Leidy Quenamá</h5>
 
-			<h4>Jorge Mendua</h4>
+			<h5>Jorge Mendua</h5>
 
 
-			<h3>Other outside collaborators</h3>
+			<h4>Other outside collaborators</h4>
 
-			<h4>Chelsea Sanker, PhD</h4>
-			<p><a href="https://www.chelseasanker.com/">chelseasanker.com</a><br />
-			<a href="mailto:chelsea.sanker@yale.edu">chelsea.sanker@yale.edu</a></p>
+			<p><b>Chelsea Sanker, PhD</b><br />
+			<a href="https://www.chelseasanker.com/">chelseasanker.com</a><br />
+			chelsea.sanker at yale.edu</p>
 			<p>Chelsea Sanker is a lecturer at Yale University. Her research focuses on synchronic patterns of details in perception and production, and reconstructions of how shifts in cue usage can lead to diachronic change.</p>
 
-			<h4>Nick Emlen, PhD</h4>
-			<p>n.q.emlen at hum.leidenuniv.nl</p>
+			<p><b>Nick Emlen, PhD</b><br />
+			n.q.emlen at hum.leidenuniv.nl</p>
 			<p>Nick Emlen is a postdoc at Leiden University. He studies linguistic, social, economic, and historical relationships that link the Andes and Amazonia, and is currently a part of the Linguistic Past of Mesoamerica and the Andes project.</p>
 
-			<h4></h4>
-			<p><a href="https://irinavw.xyz/">irinavw.xyz</a><br />
+			<p><b>Irina Wang, MID</b><br />
+			<a href="https://irinavw.xyz/">irinavw.xyz</a><br />
 			hello at irinawang.com</p>
 			<p>Irina is a designer and writer that has combined an interest in typography, literature, and environmental theory to design alongside native communities and academic linguists to revitalize endangered languages such as Marma (Chittagong Hill Tracts), Unangam Tunuu (Aleutian Islands), and A’ingae (Sucumbíos Province). Recognizing that language justice, environmental justice, and social justice are one demand arising from structural issues, she pursued a Master of Industrial Design at RISD to develop transdisciplinary approaches for ethical and urgent systems-shifting. She currently practices as Entropy Studio, where she works on and writes about design philosophy, nuclear non-proliferation, language sovereignty, energy democracy, agricultural carbon sequestration, circular food systems, urban air mobility, and post-growth governance.</p>
 
-			<h4></h4>
+			<h5></h5>
 			<p></p>
 
-			<h4></h4>
+			<h5></h5>
 			<p></p>
 
-			<h4></h4>
+			<h5></h5>
 			<p></p>
 
 
-			<h3>Lead research assistant</h3>
+			<h4>Lead research assistant</h4>
 
-			<h4>Maksymilian Dąbkowski</h4>
+			<h5>Maksymilian Dąbkowski</h5>
 			<p><a href="https://maksymilian-dabkowski.github.io/">maksymilian-dabkowski.github.io</a><br />
 			maksymilian.michal.dabkowski at gmail.com</p>
 			<p></p>
 
 
-			<h3>Current undergraduates</h3>
+			<h4>Current undergraduates</h4>
 
-			<h4>Valerie Aguilar Dellisanti</h4>
-			<h4>Corrin Anderer</h4>
-			<h4>Atabong Khumbah</h4>
-			<h4>Bryce Massey</h4>
-			<h4>Alexis Roman</h4>
-			<h4>Jack Walker</h4>
-
-
-			<h3>Former undergraduates</h3>
-
-			<h4>Justin Bai</h4>
-			<h4>Rocket Drew</h4>
-			<h4>Amyra Durkin</h4>
-			<h4>Zackary Entwistle</h4>
-			<h4>Sandy Greenberg</h4>
-			<h4>Rachel Gutman</h4>
-			<h4>Sabrina Morvillo</h4>
-			<h4>Thomas Ottaway</h4>
-			<h4>Ethan Pan</h4>
-			<h4>Kalinda Pride</h4>
-			<h4>Chiara Repetti-Ludlow</h4>
-			<h4>Nicholas Tomlin</h4>
-			<h4>Haoru Zhang</h4>
-			<h4>Jacob Zimmerman</h4>
+			<h5>Valerie Aguilar Dellisanti</h5>
+			<h5>Corrin Anderer</h5>
+			<h5>Atabong Khumbah</h5>
+			<h5>Bryce Massey</h5>
+			<h5>Alexis Roman</h5>
+			<h5>Jack Walker</h5>
 
 
+			<h4>Former undergraduates</h4>
 
-			<h2>Funding</h2>
+			<b>Justin Bai<br />
+			Rocket Drew<br />
+			Amyra Durkin<br />
+			Zackary Entwistle<br />
+			Sandy Greenberg<br />
+			Rachel Gutman<br />
+			Sabrina Morvillo<br />
+			Thomas Ottaway<br />
+			Ethan Pan<br />
+			Kalinda Pride<br />
+			Chiara Repetti-Ludlow<br />
+			Nicholas Tomlin<br />
+			Haoru Zhang<br />
+			Jacob Zimmerman</b>
+
+
+
+
+			<h3>Funding</h3>
 
 			<p>In 2017, ALDP received funding from Endangered Language Documentation Programme (ELDP <span style={{fontFamily: "monospace"}}>#SG0481</span>, <i>Kofán Collaborative Project: Collection of Audio-Video Materials and Texts)</i>.</p>
 
@@ -138,13 +139,13 @@ export function AboutPage() {
 
 
 
-			<h2>Research output</h2>
+			<h3>Research output</h3>
 
 			<p>[This section will be completed in the coding phase]</p>
 
 
 
-			<h2>Publicity</h2>
+			<h3>Publicity</h3>
 
 			<p><a href="https://www.browndailyherald.com/2019/02/20/professor-students-help-preserve-cofan-language/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+BrownDailyHerald+%28The+Brown+Daily+Herald+RSS%29">Professor, students help to preserve Cofán language | The Brown Daily Herald</a></p>
 
