bbe8c2d631f6d6dfc20abad9eb5e7f219cc2dc65
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-28 11:14:37 -0400

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function AboutPage() {
   return (
-		<div style={{margin: "0.25in"}}>
+		<div style={{margin: "0-0.25in"}}>
 
       		<h2>About ALDP <br />
 			<i style={{fontSize: "medium"}}>English</i></h2>
@@ -23,7 +23,7 @@ export function AboutPage() {
 
 			<h3>The A’ingae language</h3>
 
-			<p>While the language has more commonly been referred to by the name “Cofán,” or occasionally “Kofán” in scholarly literature, “A’ingae” is the name speakers use for their language (<i>a’ingae</i> literally means “in the way of the A’i/people”). A’ingae is a language isolate, which means that it is believed to be unrelated to any other known language on earth. Despite having no genetic relationship to other languages, A'ingae has many typological features characteristic of other languages of the region, including SOV basic word order, nominal classifiers, presence of frustative, apprehensive, and evidential morphology, highly complex agglutinative morphology, contrastive nasality on vowels and other prototypically Amazonian phonological features.</p>
+			<p>While the language has more commonly been referred to by the name “Cofán” or “Kofán” in scholarly literature, “A’ingae” is the name speakers use for their language (<i>a’ingae</i> literally means “in the way of the A’i/people”). A’ingae is a language isolate, which means that it is believed to be unrelated to any other known language on earth. Despite having no genetic relationship to other languages, A'ingae has many typological features characteristic of other languages of the region, including SOV basic word order, nominal classifiers, presence of frustative, apprehensive, and evidential morphology, highly complex agglutinative morphology, contrastive nasality on vowels and other prototypically Amazonian phonological features.</p>
 
 			<p>A’ingae is an endangered language thought to have around 2,000 native speakers in Ecuador and Colombia. The national borders are thought to demarcate a linguistic divide: of the two major dialects, one is spoken along the Aguarico River in Ecuador; the other—on the San Miguel, Guamués, and Putumayo rivers in Colombia. As the Cofán population nearly quintupled since the 1960s and A’ingae is still used vigorously, the language is classified as developing. In Ecuador, A’ingae is spoken widely though not universally across registers, by all social groups and in all domains of life, with the attitudes towards the language being positive. In communities with stronger Kichwa and colonial influences, especially in Colombia, the prominence of Spanish and other languages may be more prominent. </p>
 
@@ -66,79 +66,78 @@ export function AboutPage() {
 
 			<h3>Co-directors</h3>
 
-			<p style={{marginBottom: ".25em"}}><b>Scott anderBois</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Scott anderBois</b><br />
 			<a href="http://research.clps.brown.edu/anderbois/">research.clps.brown.edu/anderbois</a><br />
 			scott_anderbois at brown.edu</p>
 			<p>Scott is an Associate Professor in the Department of Cognitive, Linguistic, and Psychological Sciences (CLPS) at Brown University. His research is focused on issues in semantics and pragmatics, especially those concerning non-truth conditional meanings and the way they interact with sentence type. Beyond A’ingae, his work has explored these issues through primary fieldwork on Yucatec Maya (Mayan, Mexico) and Tagalog (Austronesian, Philippines). Additionally, he co-directs with Miguel Oscar Chan Dzul (Universidad de Oriente) <a href="https://yucatecmaya.github.io/LingView/#/">U koorpusil maaya t'aan</a>, a multipurpose digital corpus of Yucatec Maya.</p>
 
-			<p style={{marginBottom: ".25em"}}><b>Wilson Silva</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Wilson Silva</b><br />
 			wdelimasilva at email.arizona.edu</p>
 			<p>Wilson is an Assistant Professor in the Department of Linguistics at the University of Arizona. He is a field linguist with formal training in theoretical linguistics and language documentation & revitalization. Beyond A’ingae, he has conducted research in Desano and Siriano; two endangered Eastern Tukanoan languages spoken in the Vaupés Region of Brazil and Colombia.</p>
 
 
 			<h3>Community partners</h3>
 
-			<p style={{marginBottom: ".25em"}}><b>Hugo Lucitante</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Hugo Lucitante</b></p>
 			<p>Hugo Lucitante is a PhD student in Anthropology at UT San Antonio and a Brown University graduate from Zábalo, a small Cofán community in Ecuador. He received the Royce Fellowship in 2016 and developed the. Hugo's work was immensely important in starting this project.</p>
 
-			<p style={{marginBottom: ".25em"}}><b>Sadie Lucitante</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Shen Aguinda</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Leidy Quenamá</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Jorge Mendua</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Sadie Lucitante</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Shen Aguinda</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Leidy Quenamá</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Jorge Mendua</b></p>
 
 
 			<h4>Other outside collaborators</h4>
 
-			<p style={{marginBottom: ".25em"}}><b>Chelsea Sanker, PhD</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Chelsea Sanker, PhD</b><br />
 			<a href="https://www.chelseasanker.com/">chelseasanker.com</a><br />
 			chelsea.sanker at yale.edu</p>
 			<p>Chelsea Sanker is a lecturer at Yale University. Her research focuses on synchronic patterns of details in perception and production, and reconstructions of how shifts in cue usage can lead to diachronic change.</p>
 
-			<p style={{marginBottom: ".25em"}}><b>Nick Emlen, PhD</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Nick Emlen, PhD</b><br />
 			n.q.emlen at hum.leidenuniv.nl</p>
 			<p>Nick Emlen is a postdoc at Leiden University. He studies linguistic, social, economic, and historical relationships that link the Andes and Amazonia, and is currently a part of the Linguistic Past of Mesoamerica and the Andes project.</p>
 
-			<p style={{marginBottom: ".25em"}}><b>Irina Wang, MID</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Irina Wang, MID</b><br />
 			<a href="https://irinavw.xyz/">irinavw.xyz</a><br />
 			hello at irinawang.com</p>
 			<p>Irina is a designer and writer that has combined an interest in typography, literature, and environmental theory to design alongside native communities and academic linguists to revitalize endangered languages such as Marma (Chittagong Hill Tracts), Unangam Tunuu (Aleutian Islands), and A’ingae (Sucumbíos Province). Recognizing that language justice, environmental justice, and social justice are one demand arising from structural issues, she pursued a Master of Industrial Design at RISD to develop transdisciplinary approaches for ethical and urgent systems-shifting. She currently practices as Entropy Studio, where she works on and writes about design philosophy, nuclear non-proliferation, language sovereignty, energy democracy, agricultural carbon sequestration, circular food systems, urban air mobility, and post-growth governance.</p>
 
 
-
 			<h3>Lead research assistant</h3>
 
-			<p style={{marginBottom: ".25em"}}><b>Maksymilian Dąbkowski</b><br />
+			<p style={{marginBottom: "-0.25em"}}><b>Maksymilian Dąbkowski</b><br />
 			<a href="https://maksymilian-dabkowski.github.io/">maksymilian-dabkowski.github.io</a><br />
 			maksymilian.michal.dabkowski at gmail.com</p>
-			<p></p>
+			<p>Maksymilian is a linguistics lead research assistant at Brown University. He holds B.A. degrees in Linguistics and Logic from Brown University. He works predominantly on A'ingae, an Amazonian language isolate spoken in northeast Ecuador and southern Colombia. Maksymilian's research interests include phonology and its interfaces, semantics, language documentation, language revitalization, logic, and psycholinguistics.</p>
 
 
 			<h3>Current undergraduates</h3>
 
-			<p style={{marginBottom: ".25em"}}><b>Valerie Aguilar Dellisanti</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Corrin Anderer</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Atabong Khumbah</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Bryce Massey</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Alexis Roman</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Jack Walker</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Valerie Aguilar Dellisanti</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Corrin Anderer</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Atabong Khumbah</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Bryce Massey</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Alexis Roman</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Jack Walker</b></p>
 
 
 			<h4>Former undergraduates</h4>
 
-			<p style={{marginBottom: ".25em"}}><b>Justin Bai</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Rocket Drew</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Amyra Durkin</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Zackary Entwistle</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Sandy Greenberg</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Rachel Gutman</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Sabrina Morvillo</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Thomas Ottaway</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Ethan Pan</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Kalinda Pride</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Chiara Repetti-Ludlow</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Nicholas Tomlin</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Haoru Zhang</b></p>
-			<p style={{marginBottom: ".25em"}}><b>Jacob Zimmerman</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Justin Bai</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Rocket Drew</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Amyra Durkin</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Zackary Entwistle</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Sandy Greenberg</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Rachel Gutman</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Sabrina Morvillo</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Thomas Ottaway</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Ethan Pan</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Kalinda Pride</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Chiara Repetti-Ludlow</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Nicholas Tomlin</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Haoru Zhang</b></p>
+			<p style={{marginBottom: "-0.25em"}}><b>Jacob Zimmerman</b></p>
 			
 		</div>
   );
