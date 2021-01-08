2e4ca8924e0dafddcabb1c8e071701c57090219c
Kalinda Pride (XPS) <kalinda.pride@gmail.com>
2021-01-07 22:37:38 -0800

    no link when materials URL is missing

--- jsx/App/Materials/MaterialCard.jsx
+++ jsx/App/Materials/MaterialCard.jsx
@@ -4,10 +4,14 @@ export function MaterialCard({ metadata }) {
   return (
     <div className="material-card">
       <main>
-        <a href={metadata.itemServerUrl}><h3>{metadata.title}</h3></a>
+        {metadata.itemServerUrl ?
+          <a href={metadata.itemServerUrl}><h3>{metadata.title}</h3></a>
+        :
+          <h3>{metadata.title}</h3>
+        }
         <h4>{metadata.credits.concat([metadata.year]).filter(x => x).join(', ')}</h4>
         <p dangerouslySetInnerHTML={{__html: metadata.descriptionHTML}} /> 
-        {/* it's okay since we sanitize ahead of time (see fetch-materials.js) */}
+        {/* it's okay since we sanitize ahead of time (see fetch_materials.js) */}
         <div>{metadata.categories.map((category, i) => <Tag key={i} text={category} />)}</div>
       </main>
     </div>
